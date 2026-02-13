4e34d2ea594f8d8247e189d7b92569e4ffa132ab
brenocaxias <brenocaxias@alu.ufc.br>
2026-02-13 19:05:40 -0300

    att

--- escola_de_musica/settings.py
+++ escola_de_musica/settings.py
@@ -92,12 +92,12 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
 # --- COMPATIBILIDADE (O Cloudinary ainda pede esta linha para o collectstatic) ---
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # --- AUTENTICAÇÃO ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

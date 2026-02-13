505a62e3c745cf197c94f07ef1d12cbdfe735763
brenocaxias <brenocaxias@alu.ufc.br>
2026-02-13 18:56:28 -0300

    att

--- escola_de_musica/settings.py
+++ escola_de_musica/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
     'cloudinary',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cursos'
 ]
 

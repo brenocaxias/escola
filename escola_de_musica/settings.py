adfe940171b2f23a558d5a40f3a51fe41b888b2c
brenocaxias <brenocaxias@alu.ufc.br>
2026-02-14 10:19:32 -0300

    att

--- escola_de_musica/settings.py
+++ escola_de_musica/settings.py
@@ -76,9 +76,7 @@ USE_TZ = True
 # --- ARQUIVOS ESTÁTICOS E MÍDIA ---
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'cursos', 'static'),
-]
+STATICFILES_DIRS = []
 WHITENOISE_IGNORE_MISSING_FILES = True
 
 MEDIA_URL = '/media/'

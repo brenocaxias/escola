f6b6a47977c2f8ca13ac895a7bf9714c9b1533c1
brenocaxias <brenocaxias@alu.ufc.br>
2026-02-14 10:15:12 -0300

    penultima alteração

--- escola_de_musica/settings.py
+++ escola_de_musica/settings.py
@@ -79,6 +79,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'cursos', 'static'),
 ]
+WHITENOISE_IGNORE_MISSING_FILES = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
